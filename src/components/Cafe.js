0a4d20e6f47aa71b43b2fc79abf8be2bdaf6a7ce
JohnsonNYC <jkow95@gmail.com>
2020-06-12 11:55:36 -0400

    cafe css is looking okay at this point

--- src/components/Cafe.js
+++ src/components/Cafe.js
@@ -1,16 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import '../App.css'
 
-class Cafe extends Component {
-    render() {
-        const {cafe} = this.props
-        // console.log(cafe.id)
-        return (
-            <div className="cafe-card" style={{border:"1px solid purple"}} onClick={()=> this.props.push(`/cafes/${cafe.id}`)}>
-                <span>{cafe.name}</span>
-                <span>{cafe.address}</span>
-            </div>
-        );
-    }
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+}))
+
+export default function Cafe(props) {
+    const classes = useStyles()
+    const { cafe } = props
+    return (
+        <div className="cafe-card" style={{ border: "1px solid purple" }} >
+            <CardMedia
+                className={classes.cardMedia}
+                image="https://source.unsplash.com/random"
+                title={cafe.name}
+            />
+            <CardContent className={classes.cardContent}>
+                <Typography gutterBottom variant="h5" component="h2">
+                    {cafe.name}
+                </Typography>
+                <Typography>
+                    Insert cafe bio in here 
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small" color="primary" onClick={() => props.push(`/cafes/${cafe.id}`)}>
+                    View
+                </Button>
+            </CardActions>
+        </div>
+    );
 }
-export default Cafe;
--- src/components/CafeContainer.js
+++ src/components/CafeContainer.js
@@ -1,22 +1,95 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Cafe from './Cafe'
 import Searchbar from './Searchbar'
 
-class CafeContainer extends Component {
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Card from '@material-ui/core/Card';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
-    render() {
-        const {cafes}=this.props
-        console.log('search',this.props.search)
-        return (
-            <div className='cafe-container'>
-                <Searchbar cafes = {cafes} searchChange={this.props.searchChange} search={this.props.search}/>
-                A L L   C A F E S
-                {cafes.map((cafe)=>
-                <Cafe key={cafe.id} cafe={cafe} {...this.props.history}/>
-                )}
-            </div>
-        );
-    }
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+}))
+
+export default function CafeContainer(props) {
+    const classes = useStyles()
+    const { cafes, search, searchChange } = props
+
+    return (
+        <React.Fragment>
+            {/* <Searchbar cafes={cafes} searchChange={searchChange} search={search} /> */}
+            <CssBaseline />
+            <AppBar position="relative">
+                <Searchbar cafes={cafes} searchChange={searchChange} search={search} />
+            </AppBar>
+            <main>
+                <div className={classes.heroContent}>
+                    <Container maxWidth="sm">
+                        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Album layout
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            Something short and leading about the collection below—its contents, the creator, etc.
+                            Make it short and sweet, but not too short so folks don't simply skip over it
+                            entirely.
+                        </Typography>
+                    </Container>
+                </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                    {/* End hero unit */}
+                    <Grid container spacing={4}>
+                        {cafes.map((cafe) => (
+                            <Grid item key={cafe.id} xs={12} sm={6} md={4}>
+                                <Card className={classes.card}>
+                                    <Cafe key={cafe.id} cafe={cafe} {...props.history}/>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+        </React.Fragment>
+    )
 }
 
-export default CafeContainer;
+{/* <div className='cafe-container'>
+                    A L L   C A F E S
+                {cafes.map((cafe) =>
+                    <Cafe key={cafe.id} cafe={cafe} {...props.history} />
+                )}
+</div> */}
